31b23860058c254c0b90a762e222ad13fda8a94a
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-10-25 14:12:23 -0700

    adjust component sizing

--- src/app/(components)/Home/RequestList.tsx
+++ src/app/(components)/Home/RequestList.tsx
@@ -19,6 +19,7 @@ export default function RequestList()
 
 const styles = StyleSheet.create({
     listWrapper: {
+        position: 'absolute',
         width: '100%',
         height: '40%',
         bottom: 0,
--- src/app/(tabs)/Home.tsx
+++ src/app/(tabs)/Home.tsx
@@ -1,14 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import RequestList from "../(components)/Home/RequestList";
 
 
 export default function Home()
 {
     return (
-        <View>
+        <View style={styles.homeWrapper}>
             <Text>This is the home page!</Text>
 
             <RequestList />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    homeWrapper: {
+        width: '100%',
+        height: '100%',
+    }
+})
