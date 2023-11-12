ab0d3f6cdc64efd8871356ac8bb483e217ddf11a
Jeremy Smart <jeremysmart@csus.edu>
2023-11-12 13:29:54 -0800

    New Request Page gui
    Request service cards to be added later

---
+++ src/app/(components)/RequestType/SearchBar.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Text, Image } from 'react-native';
+
+export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (query: string) => {
+        setSearchQuery(query);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchInputWrapper}>
+                <Image style={styles.searchIcon} source={require('../../../assets/png/search.png')} />
+                {searchQuery === '' && <Text style={styles.placeholder}>Search for a Service</Text>}
+                <TextInput
+                    style={styles.searchInput}
+                    value={searchQuery}
+                    onChangeText={handleSearch}
+                    placeholderTextColor="#D3D3D3"
+                />
+            </View>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 10,
+        marginTop: 10,
+    },
+    searchInputWrapper: {
+        height: 50,
+        borderRadius: 25, // Increase this to make the ends more circular
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 10,
+    },
+    placeholder: {
+        position: 'absolute', // Add this line
+        left: 40, // Add this line
+        top: 15, // Adjust this value as needed
+        fontFamily: 'JBM', // Add this line
+        color: '#D3D3D3', // Add this line
+    },
+    searchIcon: {
+        position: 'absolute',
+        left: 10,
+        top: 15,
+        width: 20,
+        height: 20,
+    },
+    searchInput: {
+        height: '100%',
+        borderRadius: 25,
+        paddingLeft: 40, // Adjust this value as needed to make room for the icon
+        paddingRight: 10,
+        backgroundColor: 'transparent',
+        fontFamily: 'JBM',
+    },
+});
+
--- src/app/(request)/Type.tsx
+++ src/app/(request)/Type.tsx
@@ -1,11 +1,75 @@
-import { View, Text } from "react-native";
+import React, {useEffect, useState} from 'react';
+import { View, StyleSheet, Dimensions, TouchableOpacity, Image, Text, ScrollView } from 'react-native';
+import CustomText from '../(components)/CustomText';
+import { useNavigation } from 'expo-router';
+import { global } from "../../dummy";
+import SearchBar from '../(components)/RequestType/SearchBar';
+
 
 
 export default function Type()
 {
+    const nav = useNavigation()
     return (
-        <View style={{ flex: 1, backgroundColor: '#1f1f1f'}}>
-            <Text>Select request type.</Text>
+        <View style={styles.mainWrapper}>
+            <View style={styles.exitWrapper}>
+                <View style={styles.innerExitWrapper}>
+                    <Text style={styles.barText}>Select A Service</Text>
+                    <TouchableOpacity onPress={() => { nav.goBack() }}> 
+                        <Image style={styles.resizeIcon} source={require('../../assets/png/exit_x.png')} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <SearchBar />
+            <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                    <Text style={styles.serviceCardPlaceholder}>
+                        Modular service card UI goes here (WIP).
+                    </Text>
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainWrapper: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: global.baseBackground100,
+    },
+    exitWrapper:{
+        backgroundColor: global.baseBlue100,
+        width:'100%',
+        shadowColor: '#000',
+        shadowOffset:{
+            width:-2,
+            height:2,
+        },
+        shadowOpacity:0.25,
+        shadowRadius:4,
+        elevation:5,
+        height:'12%',
+    },
+    innerExitWrapper:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginRight:'6%',
+        marginTop:'13%',
+    },
+    barText: {
+        fontSize: 25,
+        marginLeft:'5%',
+        fontFamily:'JBM',
+        color:global.baseBackground100
+    },
+    resizeIcon:{
+        width:30,
+        height:30,
+    },
+    serviceCardPlaceholder: {
+        fontSize: 25, 
+        fontFamily: 'JBM', 
+        color: '#000000', 
+        textAlign: 'center', 
+        marginTop: 15,
+    },
+});
