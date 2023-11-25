0b58bee2cb849e2720f189a7d91a2e6d4502df72
JeffBianucci <bianuccij@ymail.com>
2023-11-25 12:00:44 -0800

    added more profile page functionality

--- src/app/NavApp.tsx
+++ src/app/NavApp.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from './(tabs)/Profile';
 import Profile2 from './(tabs)/Profile2';
+import Profile3 from './(tabs)/Profile3';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Profile2" component={Profile2} />
+        <Stack.Screen name="Profile3" component={Profile3} />
       </Stack.Navigator>
     </NavigationContainer>
   );
