26048b24abf7e2e58e366ee0242c46b5e594af9a
Robby <113494281+ranjotdharni@users.noreply.github.com>
2023-12-23 13:27:04 -0800

    further performace improvements to map markers

--- src/app/(components)/CustomMarker.tsx
+++ src/app/(components)/CustomMarker.tsx
@@ -1,6 +1,5 @@
 import { Marker } from "react-native-maps";
 import { responseType } from "../../customs";
-import { Image } from "react-native";
 
 export default function CustomMarker({ markerData, image, passUp } : { markerData: responseType, image: string, passUp: (obj: responseType) => void }) {
     if (markerData.geometry === undefined) {    // ensure valid geometry exists
@@ -10,8 +9,7 @@ export default function CustomMarker({ markerData, image, passUp } : { markerDat
     }
 
     return (
-        <Marker image={{uri: image}} coordinate={{latitude: markerData.geometry.y, longitude: markerData.geometry.x}} onPress={() => {passUp(markerData)}}>
-    
+        <Marker tracksViewChanges={false} icon={{uri: image}} coordinate={{latitude: markerData.geometry.y, longitude: markerData.geometry.x}} onPress={() => {passUp(markerData)}}>
         </Marker>
     )
 }
