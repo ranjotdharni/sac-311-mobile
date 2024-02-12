ca6e913eb0293fee94c2470ce723ec13d7463877
SeanAgarrado <112983348+SeanAgarrado@users.noreply.github.com>
2024-02-11 19:40:26 -0800

    Created the business tab for the profile page with its basic functions.

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -19,6 +19,7 @@ export default function Layout()
                     <Stack.Screen name="Profile3" options={{ headerShown: false }} />
                     <Stack.Screen name="Profile0" options={{ headerShown: false }} />
                     <Stack.Screen name="FAQFullView" options={{ headerShown: false }} />
+                    <Stack.Screen name="Business" options = {{ headerShown: false }} />
                 </Stack>
             <NavBar />
         </>
